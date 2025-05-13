42eeca5d3b73d26abb82e86856d8144d76bb627a
J-max <joelmtn7@gmail.com>
2025-05-13 22:12:05 +0100

    local storage

---
+++ notebook/note.js
@@ -0,0 +1,31 @@
+const noteContainer = document.querySelector('.notes-container');
+
+const createBtn = document.querySelector('.btn');
+
+let notes = document.querySelector('.input-box');
+
+createBtn.addEventListener('click', ()=> {
+    let inputBox = document.createElement('p');
+    let img = document.createElement('img');
+    inputBox.className = 'input-box';
+    inputBox.setAttribute('contenteditable', true);
+    img.src = 'img/delete.svg' 
+    noteContainer.appendChild(inputBox).append(img);
+})
+
+function updateStorage() {
+    localStorage.setItem("notes", noteContainer.innerHTML)
+}
+
+
+noteContainer.addEventListener('click', (e) => {
+    if (e.target.tagName === 'IMG') {
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+
+    else if(e.target === 'p') {
+        notes = document.querySelectorAll('.input-box');
+    }
+    
+} )
